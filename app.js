c5717f4a2980bbe9f0614f6894c623cc3e732820
Tuan Hoang <hoang.tuan@outlook.com>
2016-08-04 13:41:38 -0700

    Add game colors difficulty 3 and 6 colors mode

--- app.js
+++ app.js
@@ -1,18 +1,52 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
-var colorDispaly = document.querySelector("#colorDisplay");
+var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn"); 
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function (){
+  hardBtn.classList.remove("selected");
+  easyBtn.classList.add("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.background = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+});
+
+hardBtn.addEventListener("click", function (){
+  hardBtn.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+      squares[i].style.background = colors[i];
+      squares[i].style.display = "block";   
+  }
+});
 
 resetButton.addEventListener("click", function() {
   //generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   //pick new random colors
   pickedColor = pickColor(); 
   //change colorDisplay to match picked Color
-  colorDispaly.textContent = pickedColor;
+  colorDisplay.textContent = pickedColor;
+  this.textContent = "New Colors"
+  messageDisplay.textContent = "";
   //change colors of squares
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.background = colors[i];
@@ -20,7 +54,7 @@ resetButton.addEventListener("click", function() {
   h1.style.background = "steelblue"; 
 });
 
-colorDispaly.textContent = pickedColor;
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
   //add initial colors to squares
@@ -33,6 +67,7 @@ for (var i = 0; i < squares.length; i++) {
     console.log(clickedColor, pickedColor);
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
+      
       changeColor(clickedColor);
       h1.style.background = clickedColor;
       resetButton.textContent = "Play Again"
